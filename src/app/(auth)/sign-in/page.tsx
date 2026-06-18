160ec428bc6832bc317df077570be9858aa9a2cc
SAM <waliurrafiqsami@gmail.com>
2026-06-18 12:04:27 +0600

    solve routing

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -70,7 +70,6 @@ export default function SignInForm() {
           description: "Successfully signed in.",
         });
 
-        router.replace("/dashboard");
         router.push("/dashboard");
         router.refresh();
       }
